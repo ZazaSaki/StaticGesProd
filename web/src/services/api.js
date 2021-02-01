690b071540b27530dc84935c4726836ed9a6c79b
Zaza Saki <isac.cruzn@gmail.com>
2021-02-01 18:49:27 +0000

    backend url

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL : 'https://static-ges-backend.herokuapp.com/'
+    baseURL : 'https://withoutlogin.herokuapp.com/'
 });
 
 export default api;
